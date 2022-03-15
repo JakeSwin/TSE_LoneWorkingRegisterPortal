a37c821525c84d6e3eff862057ef388e8b5b20a3
Jacob Swindell <jacob.swindell@btinternet.com>
2022-03-15 11:56:56 +0000

    add graph + taskbar

---
+++ src/components/TaskBar.vue
@@ -0,0 +1,57 @@
+<script>
+
+</script>
+
+<template>
+    <div class="task-bar-content">
+        <div class="bar">
+            <object data="/task-bar-top.svg" type="image/svg+xml"></object>
+            <img src="@/assets/LincolnLogo.png" alt="" class="logo">
+            <h2>Sign Out</h2>
+        </div>
+        <div class="invis">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.task-bar-content {
+    width: 375px;
+    height: 667px;
+    display: flex;
+    position: fixed;
+    z-index: 100;
+}
+
+.bar {
+    width: 236px;
+    height: 100%;
+    background-color: #002147;
+    padding: 0;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+}
+
+.logo {
+    width: 120px;
+    position: fixed;
+    left: 5px;
+    top: 5px;
+}
+
+.bar h2 {
+    color: white;
+    margin: 15px;
+    font-size: 1.3em;
+}
+
+.invis {
+    width: 60%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,8 +1,51 @@
 <script>
+import TaskBar from '../components/TaskBar.vue'
 
+export default {
+    mounted() {
+        var colorscaleValue = [
+            [0, "#EBEBEB"],
+            [1, "#33E9C8"]
+        ];
+        var data = [
+            {
+                z: [
+                    [0, 1, 1, 0, 0, 0, 1],
+                    [1, 0, 1, 0, 0, 1, 0],
+                    [0, 0, 0, 1, 0, 0, 1],
+                    [0, 0, 1, 0, 0, 0, 0]
+                ],
+                x: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+                y: [1, 2, 3, 4],
+                xgap: 5,
+                ygap: 5,
+                type: "heatmap",
+                colorscale: colorscaleValue,
+                showscale: false
+            }
+        ];
+        var config = {
+            staticPlot: true
+        };
+        var layout = {
+            showLegend: false,
+            width: 350,
+            height: 320,
+            margin: {
+                t: 0,
+                b: 60,
+                l: 30,
+                r: 30
+            }
+        };
+        Plotly.newPlot("sign-in-heatmap", data, layout, config);
+    },
+    components: { TaskBar }
+}
 </script>
 
 <template>
+  <TaskBar></TaskBar>
   <div class="container">
     <header>
       <object data="/MenuIcon.svg" type="image/svg+xml"></object>
@@ -12,8 +55,8 @@
       <div class="sign-in-status status-on">
         <p>You are currently signed in Room INB1102</p>
       </div>
-      <div class="sign-in-heatmap">
-      
+      <div id="sign-in-heatmap">
+        
       </div>
       <div class="rooms">
         <p>Update Room:</p>
@@ -53,7 +96,7 @@ header {
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 3em;
+  gap: 2.5em;
   font-size: 1rem;
   background-color: white;
   width: 100%;
