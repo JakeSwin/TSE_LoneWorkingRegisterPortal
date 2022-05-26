6c9b61e9881f3c412fc191c86daa23bcbd3963e1
Jacob Swindell <jacob.swindell@btinternet.com>
2022-05-26 14:30:48 +0100

    add heatmap fix

--- src/components/statistics/Heatmap.vue
+++ src/components/statistics/Heatmap.vue
@@ -1,15 +1,5 @@
 <script>
 export default {
-  data() {
-    return {
-      heatmapData: [
-          [0, 1, 0, 0, 0, 0, 1],
-          [0, 0, 1, 0, 0, 1, 0],
-          [0, 0, 0, 1, 0, 0, 0],
-          [0, 1, 0, 1, 0, 0, 1]
-        ]
-    }
-  },
   props: {
     studentID: {
       type: String,
@@ -24,45 +14,45 @@ export default {
       res.json()
       .then(json => {
         console.log(json)
-        this.heatmapData = json
+        const heatmapData = json
+
+        var colorscaleValue = [
+          [0, "#EBEBEB"],
+          [1, "#33E9C8"]
+        ];
+        var data = [
+          {
+            z: heatmapData,
+            x: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            y: [1, 2, 3, 4],
+            xgap: 5,
+            ygap: 5,
+            type: "heatmap",
+            colorscale: colorscaleValue,
+            showscale: false
+          }
+        ];
+        var config = {
+            staticPlot: true
+        };
+        var layout = {
+            showLegend: false,
+            width: 350,
+            height: 320,
+            margin: {
+                t: 0,
+                b: 60,
+                l: 30,
+                r: 30
+            }
+        };
+        try {
+        Plotly.newPlot("sign-in-heatmap", data, layout, config);
+        } catch {
+          
+        }
       })
     })
-
-    var colorscaleValue = [
-        [0, "#EBEBEB"],
-        [1, "#33E9C8"]
-    ];
-    var data = [
-      {
-        z: this.heatmapData,
-        x: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        y: [1, 2, 3, 4],
-        xgap: 5,
-        ygap: 5,
-        type: "heatmap",
-        colorscale: colorscaleValue,
-        showscale: false
-      }
-    ];
-    var config = {
-        staticPlot: true
-    };
-    var layout = {
-        showLegend: false,
-        width: 350,
-        height: 320,
-        margin: {
-            t: 0,
-            b: 60,
-            l: 30,
-            r: 30
-        }
-    };
-    try {
-    Plotly.newPlot("sign-in-heatmap", data, layout, config);
-    } catch {
-      
-    }
   },
 }
 </script>
